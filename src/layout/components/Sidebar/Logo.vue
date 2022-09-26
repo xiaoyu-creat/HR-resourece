077b5768043135b5892bef3b9eb09bcb9c71befc
zyy <18317693155@163.com>
2022-09-26 11:09:05 +0800

    feat:修改侧边栏样式

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="sidebar-logo-container" :class="{'collapse':collapse}">
     <transition name="sidebarLogoFade">
-      <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
+      <router-link class="sidebar-logo-link" to="/">
+        <img src="" class="sidebar-logo">
+      </router-link>
+      <!-- <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
         <img v-if="logo" :src="logo" class="sidebar-logo">
         <h1 v-else class="sidebar-title">{{ title }} </h1>
       </router-link>
       <router-link v-else key="expand" class="sidebar-logo-link" to="/">
         <img v-if="logo" :src="logo" class="sidebar-logo">
         <h1 class="sidebar-title">{{ title }} </h1>
-      </router-link>
+      </router-link> -->
     </transition>
   </div>
 </template>
@@ -46,7 +49,7 @@ export default {
   width: 100%;
   height: 50px;
   line-height: 50px;
-  background: #2b2f3a;
+  // background: #2b2f3a;
   text-align: center;
   overflow: hidden;
 
@@ -55,8 +58,9 @@ export default {
     width: 100%;
 
     & .sidebar-logo {
-      width: 32px;
-      height: 32px;
+      // width: 32px;
+      // height: 32px;
+      width: 110px;
       vertical-align: middle;
       margin-right: 12px;
     }
@@ -76,6 +80,8 @@ export default {
   &.collapse {
     .sidebar-logo {
       margin-right: 0px;
+      width: 32px;
+      height: 32px;
     }
   }
 }
---
+++ src/permission.js
@@ -0,0 +1,32 @@
+import router from '@/router'
+import store from '@/store'
+
+const whiteList = ['/login', '/404']
+
+// 路由前置守卫
+// 1. 权限拦截处理逻辑需要放在路由的前置守卫里处理
+// 2. 首先判断有无token
+// 2.1 token 存在 说明 处于登录状态
+// 2.1.1 判断是否处于登录页面，是的话 跳首页 否则 直接留在当前页（直接放行 next()）
+// 2.2 token 不存在 说明 不处于登录状态
+// 2.2.1 判断一下 是否处于白名单 是的话 直接留在 当前页 否则 跳转到登录页
+router.beforeEach((to, from, next) => {
+//   console.log(to)
+//   console.log(from)
+//   next(false)
+  if (store.getters.token) { // token 是否存在
+    if (to.path === '/login') { // 判断是否在登录页
+      next('/')
+    } else {
+      next()
+    }
+  } else { // token 不存在
+    // includes() 方法用于判断字符串是否包含指定的子字符串，或者判断数组中是否有指定的元素。
+    // 例如：[‘hellow’,‘world’].includes(‘hellow’) ,如果数组存在指定元素就会返回true，没有就返回false。
+    if (whiteList.includes(to.path)) { // 判断是否处于白名单
+      next()
+    } else {
+      next('/login')
+    }
+  }
+})
